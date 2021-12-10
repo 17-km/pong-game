853e3838bef998b3cde8c0637c98b21dcff2297b
17-km <krystian.mikoda.programista@gmail.com>
2021-12-10 03:04:14 +0100

    Added paddles control

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,27 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *backgroundForMatch;
+        TShape *rightPaddle;
+        TTimer *rightPaddleTop;
+        TTimer *rightPaddleDown;
+        TShape *leftPaddle;
+        TTimer *leftPaddleDown;
+        TTimer *leftPaddleTop;
+        void __fastcall rightPaddleDownTimer(TObject *Sender);
+        void __fastcall rightPaddleTopTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall leftPaddleDownTimer(TObject *Sender);
+        void __fastcall leftPaddleTopTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
@@ -19,4 +36,3 @@ public:		// User declarations
 extern PACKAGE TForm1 *Form1;
 //---------------------------------------------------------------------------
 #endif
- 
